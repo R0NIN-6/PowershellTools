e6971b1254fbc5e89eedbe5026a55ddd65ff01fe
ac-msft <44343527+ac-msft@users.noreply.github.com>
2021-07-14 09:17:22 -0500

    Added PDF/Word Scripts
    Added Scripts for PDF and Word text extraction.

---
+++ Get-PDFSummary.ps1
@@ -0,0 +1,55 @@
+﻿## Extract Table of Contents (plus figures and tables) from a PDF Document via Powershell ##
+Param 
+(
+    [Parameter(Mandatory = $True)]
+    [ValidateNotNullOrEmpty()]
+    [String]
+    $FileName   
+)
+Function convert-PDFtoText {
+	Param(
+		[Parameter(Mandatory=$true)][string]$file
+	)	
+	Add-Type -Path "C:\DLLs\itextsharp.dll"
+    $file = $file.trim('"') 
+	$pdf = New-Object iTextSharp.text.pdf.pdfreader -ArgumentList $file
+    $bm = [iTextSharp.text.pdf.SimpleBookmark]::GetBookmark($pdf)
+    #sub objects are in "Kids" property
+    $bookmarks = [iTextSharp.text.pdf.SimpleBookmark]::GetBookmark($pdf)
+    $PageCount = $pdf.NumberOfPages
+    Write-Host "Number of Pages: $PageCount"
+    if($bookmarks -ne $NULL)
+    {
+        Write-host "Bookmarks: `n"
+        foreach($bookmark in $bookmarks.Kids){Write-Host $bookmark.Title}
+    }
+	for ($page = 1; $page -le $pdf.NumberOfPages; $page++){
+		$text=[iTextSharp.text.pdf.parser.PdfTextExtractor]::GetTextFromPage($pdf,$page)
+		Write-Output $text 
+	}	
+	$pdf.Close()
+}
+#Call the function above
+$FullText = convert-PDFtoText $FileName 
+$TotalLines = $File.Count
+$Dictionary = @{}
+$LineCount = 0
+$FullText | foreach {
+    $Line = $_
+    $LineCount++
+    Write-Progress -Activity "Processing words..." `
+    -PercentComplete ($LineCount*100/$TotalLines)     
+    $Line -split "[^a-zA-Z]" | foreach {
+        $Word = $_.ToUpper()
+        If ($Word[0] -ge 'A' -and $Word[0] -le "Z") {
+            $WordCount++
+            If ($Dictionary.ContainsKey($Word)) {
+                $Dictionary.$Word++
+            } else {
+                $Dictionary.Add($Word, 1)
+            }
+        }
+    } 
+}
+$Dictionary.GetEnumerator() | ? { $_.Name.Length -gt 4 } | 
+Sort Value -Descending | Select -First 10
---
+++ Get-WordSummary.ps1
@@ -0,0 +1,48 @@
+﻿## Extract Table of Contents (plus figures and tables) from a Word Document via Powershell ##
+
+# create the object for a Word Document
+$Word_obj = New-Object -comobject Word.Application
+#Show/hide the word application 
+$Word_obj.Visible = $False
+#Select file via browser
+Add-Type -AssemblyName System.Windows.Forms
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog
+[void]$FileBrowser.ShowDialog()
+$file=$FileBrowser.FileName
+
+$doc = $Word_obj.Documents.Open($file) 
+$FullText = $doc.Paragraphs | ForEach-Object {
+    $_.Range.Text
+}
+Write-Host "Word count is $($doc.Words.Count)"
+#Type 88 is hyperlinks
+foreach($f in $doc.Fields){
+     if ($f.Type -eq 88){$f.Result.Text
+     }}
+$TotalLines = $FullText.Count
+$Dictionary = @{}
+$LineCount = 0
+$FullText | foreach {
+    $Line = $_
+    $LineCount++
+    Write-Progress -Activity "Processing words..." `
+    -PercentComplete ($LineCount*100/$TotalLines)     
+    $Line -split "[^a-zA-Z]" | foreach {
+        $Word = $_.ToUpper()
+        If ($Word[0] -ge 'A' -and $Word[0] -le "Z") {
+            $WordCount++
+            If ($Dictionary.ContainsKey($Word)) {
+                $Dictionary.$Word++
+            } else {
+                $Dictionary.Add($Word, 1)
+            }
+        }
+    } 
+}
+
+$Dictionary.GetEnumerator() | ? { $_.Name.Length -gt 4 } | 
+Sort Value -Descending | Select -First 10
+
+#Release the COM variable and terminate Word
+$doc.close()
+$Word_obj.Quit()
